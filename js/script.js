7579ce723655c7262b5288ed8188260b14f6fefb
MatiushkoDasha <zmiyka47gmail.com>
2025-10-07 19:02:45 +0300

    hata plus media

---
+++ js/script.js
@@ -0,0 +1,9 @@
+dzvin.onclick =  () => {
+    popup.style.display ="block"
+    darck.style.display ="block"
+}
+
+darck.onclick =  () => {
+    popup.style.display ="none"
+    darck.style.display ="none"
+}
